d953c000eb5ac5cb13462502b9345f4f988cea35
UlrikeWerner <ulrike_werner@web.de>
2022-10-27 18:08:18 +0200

    add cubes to useStore

--- src/hooks/useStore.js
+++ src/hooks/useStore.js
@@ -3,21 +3,10 @@ import create from "zustand";
 
 export const useStore = create((set) => ({
   texture: "dirt",
-  cubes: [
-    {
-      key: nanoid(),
-      pos: [2, 0.5, 2],
-      texture: "dirt",
-    },
-    {
-      key: nanoid(),
-      pos: [1, 0.5, 2],
-      texture: "wood",
-    },
-  ],
+  cubes: [],
   addCube: (x, y, z) => {
     set((prev) => ({
-      cube: [
+      cubes: [
         ...prev.cubes,
         {
           key: nanoid(),
