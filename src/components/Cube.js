e552e2f035ea269f8246e37a911a4e95dfb41cdf
UlrikeWerner <ulrike_werner@web.de>
2022-10-28 16:32:38 +0200

    adding and removing cubes

--- src/components/Cube.js
+++ src/components/Cube.js
@@ -1,4 +1,5 @@
 import { useBox } from "@react-three/cannon";
+import { useStore } from "../hooks/useStore.js";
 import * as textures from "../images/textures.js";
 
 export const Cube = ({ position, texture }) => {
@@ -7,10 +8,50 @@ export const Cube = ({ position, texture }) => {
     position,
   }));
 
+  const [addCube, removeCube] = useStore((state) => [
+    state.addCube,
+    state.removeCube,
+  ]);
+
   const activeTexture = textures[texture + "Texture"];
 
   return (
-    <mesh ref={ref}>
+    <mesh
+      onClick={(event) => {
+        event.stopPropagation();
+        const clickedFace = Math.floor(event.faceIndex / 2);
+        const { x, y, z } = ref.current.position;
+
+        if (event.altKey) {
+          removeCube(x, y, z);
+          return;
+        }
+
+        switch (clickedFace) {
+          case 0:
+            addCube(x + 1, y, z);
+            break;
+          case 1:
+            addCube(x - 1, y, z);
+            break;
+          case 2:
+            addCube(x, y + 1, z);
+            break;
+          case 3:
+            addCube(x, y - 1, z);
+            break;
+          case 4:
+            addCube(x, y, z + 1);
+            break;
+          case 5:
+            addCube(x, y, z - 1);
+            break;
+          default:
+          // do nothing
+        }
+      }}
+      ref={ref}
+    >
       <boxBufferGeometry attach="geometry" />
       <meshStandardMaterial map={activeTexture} attach="material" />
     </mesh>
